d4baa0cf458059eabb8e97b2ff766ba7b82b8738
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-10-30 21:54:06 -0200

    changed file name to be clear its meaning

--- extract-urls.sh
+++ extract-urls.sh
@@ -1,1 +1,1 @@
-cat zura-log.err | grep Forbidden | awk '{print $12}' | cut -c5-90 | cut -d "]" -f1 > urls.txt
+cat log.err | grep Forbidden | awk '{print $12}' | cut -c5-90 | cut -d "]" -f1 > urls.txt
